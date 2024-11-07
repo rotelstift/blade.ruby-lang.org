1a4411a5b75374265fe63080e5f5e640fc8193ab
rotelstift <rotelstift@gmail.com>
2024-11-07 17:53:15 +0900

    Handle cases when AWS credentials are nil.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,8 +79,8 @@ Rails.application.configure do
   Aws.config.update({
     credentials:
     (Aws::Credentials.new(
-      Rails.application.credentials.aws.access_key_id,
-      Rails.application.credentials.aws.secret_access_key
+      Rails.application.credentials.aws&.access_key_id,
+      Rails.application.credentials.aws&.secret_access_key
     ))
   })
 end
